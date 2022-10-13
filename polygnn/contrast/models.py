0c05bcc92caf2510f07daafea0424395a5c92163
rishigurnani <rgurnani96@gmail.com>
2022-10-13 13:02:58 -0400

    Add method to contrastive model that computes representation only.

--- polygnn/contrast/models.py
+++ polygnn/contrast/models.py
@@ -40,21 +40,26 @@ class preTrainContrastivePolyGNN(pt.std_module.StandardModule):
         )
 
         # set up linear blocks
-        self.mlp_head = pt.layers.Mlp(
+        self.mlp_head = pt.models.MlpOut(
             input_dim=self.mpnn.readout_dim,
             output_dim=32,
             hps=self.hps,
             debug=False,
         )
 
-        self.projection_head = pt.layers.Mlp(
+        self.projection_head = pt.models.MlpOut(
             input_dim=32,
             output_dim=32,
             hps=self.hps,
             debug=False,
         )
 
-    def forward(self, data, data_augmented):
+    def represent(self, data, data_augmented):       
+        """
+        The contents of this method are separated from `self.forward` so that
+        this block can be called in isolation during downstream tasks.
+        """
+        # Deal with data.
         x, edge_index, edge_weight, batch = (
             data.x,
             data.edge_index,
@@ -62,7 +67,10 @@ class preTrainContrastivePolyGNN(pt.std_module.StandardModule):
             data.batch
         )  # extract variables
         x = self.mpnn(x, edge_index, edge_weight, batch)
-
+        x = F.leaky_relu(x)
+        x = self.mlp_head(x)
+        
+        # Deal with data_augmented.
         x_aug, edge_index_aug, edge_weight_aug, batch_aug = (
             data_augmented.x,
             data_augmented.edge_index,
@@ -70,14 +78,20 @@ class preTrainContrastivePolyGNN(pt.std_module.StandardModule):
             data_augmented.batch
         )  # extract variables
         x_aug = self.mpnn(x_aug, edge_index_aug, edge_weight_aug, batch_aug)
-
-        x = F.leaky_relu(x)
-        x = self.mlp_head(x)
-        x = F.leaky_relu(x)
-        x = self.projection_head(x)
-
         x_aug = F.leaky_relu(x_aug)
         x_aug = self.mlp_head(x_aug)
+        return x, x_aug
+    
+    def forward(self, data, data_augmented):
+        """
+        This method can be called during pre-training.
+        """
+        x, x_aug = self.represent(data, data_augmented)
+        # Deal with data.
+        x = F.leaky_relu(x)
+        x = self.projection_head(x)
+        
+        # Deal with data_augmented.
         x_aug = F.leaky_relu(x_aug)
         x_aug = self.projection_head(x_aug)
 
