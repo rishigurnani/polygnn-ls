03dbf173ab19908a879a30cd8a54d24d1e2017dc
Rishi Gurnani <rgurnani96@gmail.com>
2022-11-02 21:47:18 -0400

    Added `polyGNN_fromPretrained` model; added test for this model; improved readability of `polyGNN` model code.

--- polygnn/models.py
+++ polygnn/models.py
@@ -23,6 +23,20 @@ class polyGNN(pt.std_module.StandardModule):
         normalize_embedding=True,
         debug=False,
     ):
+        """
+        Keyword arguments
+            node_size (int): The number of node features.
+            edge_size (int): The number of edge features.
+            selector_dim (int): The dimension of the selector vector.
+            freeze (bool): If True, the parameters of the mpnn layers
+                will be frozen. Otherwise, the mpnn layers will be
+                fine-tuned.
+            hps (HpConfig): The hyperparameters to use for
+                all layers in the model.
+            normalize_embedding (bool): If True, the node features
+                will be aggregated using the mean. Otherwise, the
+                sum will be used for aggregation.
+        """
         super().__init__(hps)
 
         self.node_size = node_size
@@ -40,32 +54,93 @@ class polyGNN(pt.std_module.StandardModule):
             debug,
         )
 
-        # set up linear blocks
-        self.final_mlp = pt.layers.Mlp(
+        # Set up the Estimator.
+        self.estimator = pt.layers.Mlp(
             input_dim=self.mpnn.readout_dim + self.selector_dim,
             output_dim=32,
             hps=self.hps,
             debug=False,
         )
-        self.out_layer = pt.layers.my_output(size_in=32, size_out=1)
+        ## Final layer of the Estimator.
+        self.final = pt.layers.my_output(size_in=32, size_out=1)
 
     def forward(self, data):
-        x, edge_index, edge_weight, batch, selector = (
+        data.x = self.mpnn(data.x, data.edge_index, data.edge_weight, data.batch)
+        data.x = F.leaky_relu(data.x)
+        data.x = torch.cat((data.x, data.selector), dim=1)
+        data.x = self.estimator(data.x)
+        data.x = self.final(data.x)
+        data.x = torch.clip(  # prevent inf and -inf
             data.x,
-            data.edge_index,
-            data.edge_weight,
-            data.batch,
-            data.selector,
-        )  # extract variables
-        x = self.mpnn(x, edge_index, edge_weight, batch)
-        x = F.leaky_relu(x)
-        x = torch.cat((x, selector), dim=1)
-        x = self.final_mlp(x)  # hidden layers
-        x = self.out_layer(x)  # output layer
-        x = torch.clip(  # prevent inf and -inf
-            x,
             min=-0.5,
             max=1.5,  # choose -0.5 and 1.5 since the output should be between 0 and 1
         )
-        x[torch.isnan(x)] = 1.5  # prevent nan
-        return x.view(data.num_graphs, 1)  # get the shape right
+        data.x[torch.isnan(data.x)] = 1.5  # prevent nan
+        return data.x.view(data.num_graphs, 1)  # get the shape right
+
+
+class polyGNN_fromPretrained(pt.std_module.StandardModule):
+    def __init__(
+        self,
+        node_size,
+        edge_size,
+        selector_dim,
+        estimator_hps,
+        mpnn,
+        freeze,
+        normalize_embedding=True,
+        debug=False,
+    ):
+        """
+        Keyword arguments
+            node_size (int): The number of node features.
+            edge_size (int): The number of edge features.
+            selector_dim (int): The dimension of the selector vector.
+            estimator_hps (HpConfig): The hyperparameters to use for
+                the Estimator.
+            mpnn (pt.std_module.StandardModule): The network with
+                the pre-trained message passing layers.
+            freeze (bool): If True, the parameters of the mpnn layers
+                will be frozen. Otherwise, the mpnn layers will be
+                fine-tuned.
+            normalize_embedding (bool): If True, the node features
+                will be aggregated using the mean. Otherwise, the
+                sum will be used for aggregation.
+        """
+        super().__init__(estimator_hps)
+        self.node_size = node_size
+        self.edge_size = edge_size
+        self.selector_dim = selector_dim
+        self.freeze = freeze
+        self.normalize_embedding = normalize_embedding
+        self.debug = debug
+
+        # Set up the MPNN.
+        self.mpnn = mpnn
+        if self.freeze:
+            for param in self.mpnn.parameters():
+                param.requires_grad = False
+
+        # Set up the Estimator.
+        self.estimator = pt.layers.Mlp(
+            input_dim=self.mpnn.mpnn.readout_dim + self.selector_dim,
+            output_dim=32,
+            hps=self.hps,
+            debug=False,
+        )
+        ## Final layer of the Estimator.
+        self.final = pt.layers.my_output(size_in=32, size_out=1)
+
+    def forward(self, data):
+        data.x = self.mpnn.represent(data)
+        data.x = F.leaky_relu(data.x)
+        data.x = torch.cat((data.x, data.selector), dim=1)
+        data.x = self.estimator(data.x)
+        data.x = self.final(data.x)
+        data.x = torch.clip(  # prevent inf and -inf
+            data.x,
+            min=-0.5,
+            max=1.5,  # choose -0.5 and 1.5 since the output should be between 0 and 1
+        )
+        data.x[torch.isnan(data.x)] = 1.5  # prevent nan
+        return data.x.view(data.num_graphs, 1)  # get the shape right
--- tests/test_polygnn.py
+++ tests/test_polygnn.py
@@ -1,6 +1,7 @@
 from polygnn_kit.polygnn_kit import LadderPolymer, LinearPol
 import pytest
-from torch import tensor, FloatTensor, nn
+import torch
+from torch_geometric.loader import DataLoader
 import polygnn_trainer as pt
 import numpy as np
 
@@ -8,7 +9,7 @@ from polygnn import __version__
 from polygnn import models
 from polygnn import layers as layers
 from polygnn import featurize as feat
-from polygnn import diff
+from polygnn import diff, contrast
 from polygnn.trimer import TrimerLadder, TrimerLinear
 
 
@@ -25,7 +26,7 @@ def example_data():
         {
             "capacity": capacity,
             "dropout_pct": 0.0,
-            "activation": nn.functional.leaky_relu,
+            "activation": torch.nn.functional.leaky_relu,
         }
     )
     train_smiles = ["[*]CC[*]", "[*]CC(C)[*]"]
@@ -50,9 +51,11 @@ def example_data():
         for x in val_smiles
     ]
     for x in train_X + val_X:
-        x.y = tensor(1.3)  # set dummy y value for each data point
-        x.selector = FloatTensor([[1, 0]]).detach()
+        x.y = torch.tensor(1.3)  # set dummy y value for each data point
+        x.selector = torch.FloatTensor([[1, 0]]).detach()
     return {
+        "hps": hps,
+        "train_smiles": train_smiles,
         "model": models.polyGNN(
             node_size=atom_config.n_features,
             edge_size=bond_config.n_features,
@@ -129,6 +132,135 @@ def test_model_training(example_data):
     )
 
     assert True
+
+
+def test_polyGNN_pretraining(example_data):
+    """
+    This test checks that freezing and fine-tuning both work.
+    """
+    bond_config = feat.BondConfig(True, True, True)
+    atom_config = feat.AtomConfig(
+        True,
+        True,
+        True,
+        True,
+        True,
+        True,
+        False,
+        True,
+    )
+    # Create an HpConfig object for the pretrained model.
+    pretrained_hps = pt.hyperparameters.HpConfig()
+    embedding_dim = pt.hyperparameters.ModelParameter(int)
+    setattr(pretrained_hps, "embedding_dim", embedding_dim)
+    pretrained_hps.set_values(
+        {
+            "capacity": 7,
+            "dropout_pct": 0.0,
+            "activation": torch.nn.functional.leaky_relu,
+            "embedding_dim": 128,
+        }
+    )
+    # Load the pretrained model.
+    pretrained = contrast.models.preTrainContrastivePolyGNN(
+        node_size=atom_config.n_features,
+        edge_size=bond_config.n_features,
+        selector_dim=None,
+        hps=pretrained_hps,
+    )
+    pretrained.load_state_dict(
+        torch.load("sample_models/simclr.pt", map_location=torch.device("cpu"))
+    )
+    # Create the downstream model.
+    selector_dim = len(example_data["scalers"])
+    downstream = models.polyGNN_fromPretrained(
+        node_size=atom_config.n_features,
+        edge_size=bond_config.n_features,
+        selector_dim=selector_dim,
+        estimator_hps=example_data["hps"],
+        mpnn=pretrained,
+        freeze=True,
+    )
+    # There is a minus 1 below since the capacity of the mlp_head is
+    # automatically decremented by 1 inside the
+    # preTrainContrastivePolyGNN object.
+    assert downstream.mpnn.mlp_head.hps.capacity.get_value() == (7 - 1)
+    # Create a trainConfig object for the downstream model.
+    train_config = pt.train.trainConfig(
+        loss_obj=pt.loss.sh_mse_loss(),
+        amp=False,
+        device="cpu",
+        epoch_suffix="",
+        multi_head=False,
+    )
+    # Create the training data.
+    ## Make the graphs for each smiles string.
+    train_X = [
+        feat.get_minimum_graph_tensor(x, bond_config, atom_config, "monocycle")
+        for x in example_data["train_smiles"]
+    ]
+    ## Manually add a selector vector and label to each graph.
+    for x in train_X:
+        x.y = torch.tensor(1.3)  # set dummy y value for each data point
+        x.selector = torch.FloatTensor([[1, 0]]).detach()
+    loader = DataLoader(train_X, batch_size=len(train_X))
+    optimizer = torch.optim.Adam(downstream.parameters(), lr=0.001)  # Adam optimization
+    # ###############################################################
+    # Test that freezing works
+    # ###############################################################
+    pretrain_weight_before = downstream.mpnn.mlp_out.linear.weight.clone().detach()
+    downstream_weight_before = downstream.final.linear.weight.clone().detach()
+    for data in loader:
+        _, loss = pt.train.minibatch(data, train_config, downstream, selector_dim)
+        loss.backward()
+        optimizer.step()
+    pretrain_weight_after = downstream.mpnn.mlp_out.linear.weight.clone().detach()
+    downstream_weight_after = downstream.final.linear.weight.clone().detach()
+    # The downstream weights should be changed after the weight update.
+    assert not torch.equal(downstream_weight_before, downstream_weight_after)
+    # The frozen pretraining weights should be the same before and
+    # after the weight update.
+    assert torch.equal(pretrain_weight_before, pretrain_weight_after)
+    # ###############################################################
+    # Test that fine-tuning works
+    # ###############################################################
+    # We need to re-load the pretrained model so that the `requires_grad`
+    # attribute of all its parameters are reset.
+    pretrained = contrast.models.preTrainContrastivePolyGNN(
+        node_size=atom_config.n_features,
+        edge_size=bond_config.n_features,
+        selector_dim=None,
+        hps=pretrained_hps,
+    )
+    pretrained.load_state_dict(
+        torch.load("sample_models/simclr.pt", map_location=torch.device("cpu"))
+    )
+    # Create the downstream model.
+    downstream = models.polyGNN_fromPretrained(
+        node_size=atom_config.n_features,
+        edge_size=bond_config.n_features,
+        selector_dim=selector_dim,
+        estimator_hps=example_data["hps"],
+        mpnn=pretrained,
+        freeze=False,
+    )
+    # Re-instantiate the optimizer so that the model parameters of the
+    # new fine-tunable model are optimized rather than the parameters
+    # of the old frozen model.
+    optimizer = torch.optim.Adam(downstream.parameters(), lr=0.001)  # Adam optimization
+    pretrain_weight_before = downstream.mpnn.mlp_out.linear.weight.clone().detach()
+    downstream_weight_before = downstream.final.linear.weight.clone().detach()
+    for data in loader:
+        _, loss = pt.train.minibatch(data, train_config, downstream, selector_dim)
+        loss.backward()
+        optimizer.step()
+    pretrain_weight_after = downstream.mpnn.mlp_out.linear.weight.clone().detach()
+    downstream_weight_after = downstream.final.linear.weight.clone().detach()
+    # Both the downstream and the pretrained weights should be changed
+    # after the weight update.
+    assert not torch.equal(pretrain_weight_before, pretrain_weight_after)
+    assert not torch.equal(downstream_weight_before, downstream_weight_after)
+    # ###############################################################
 
 
 @pytest.mark.parametrize(
