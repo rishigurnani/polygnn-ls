bd899ccd889f887316a58e921c75957335faba48
Sravan Jayanthi <sjayanthi8@gatech.edu>
2022-11-09 01:43:42 -0500

    Node deletion with parallelization

--- polygnn/contrast/node_deletion.py
+++ polygnn/contrast/node_deletion.py
@@ -1,33 +1,58 @@
 import numpy as np
 import torch
 from torch_geometric.data import Data
-from torch_geometric.data import Batch
 
 
-def _delete_node(graph, idx_to_remove):
-    # Create new node feature array
-    new_x = torch.cat(
-        [
-            graph.x[0:idx_to_remove],
-            graph.x[idx_to_remove + 1:],
-        ]
-    )
+def _delete_node(batch_graph_data, random_ids):
+    # Implement parallelization over batch
+    all_ids = torch.arange(0, len(batch_graph_data.batch))
+    # compareview = all_ids.expand(random_ids.shape[0], all_ids.shape[0]).T
+    #
+    # keep_ids = random_ids[(compareview != random_ids).T.prod(1)==1]
+    combined = torch.cat((random_ids, all_ids))
+    uniques, counts = combined.unique(return_counts=True)
+    difference = uniques[counts == 1].long()
 
-    np_edge_index = graph.edge_index.cpu().numpy()
-    np_edge_weight = graph.edge_weight.cpu().numpy()
+    batch_graph_data.x = torch.index_select(batch_graph_data.x, 0, difference)
+    batch_graph_data.batch = torch.index_select(batch_graph_data.batch, 0, difference)
 
-    # Select edges to remove that contained that node
+    np_edge_index = batch_graph_data.edge_index.numpy()
     edges_to_keep = np.invert(
-        np.logical_or(np_edge_index[0] == idx_to_remove, np_edge_index[1] == idx_to_remove)
+        np.logical_or(np.in1d(np_edge_index[0], random_ids), np.in1d(np_edge_index[1], random_ids))
     )
+    edges_to_keep = torch.tensor(np.where(edges_to_keep)[0])
 
-    np_edge_weight = np_edge_weight[edges_to_keep]
-    np_edge_index = np_edge_index[:, edges_to_keep]
+    batch_graph_data.edge_weight = torch.index_select(batch_graph_data.edge_weight, 0, edges_to_keep)
+    batch_graph_data.edge_index = torch.index_select(batch_graph_data.edge_index, 1, edges_to_keep)
+    for i, idx_to_remove in enumerate(random_ids):
+        batch_graph_data.edge_index[batch_graph_data.edge_index > idx_to_remove] -= 1
+        batch_graph_data.ptr[i+1:] -= 1
 
-    # Decrement index counter for all node with index greater than one deleted
-    np_edge_index[np_edge_index > idx_to_remove] -= 1
+    return batch_graph_data
 
-    return new_x, np_edge_index, np_edge_weight
+    # Create new node feature array
+    # new_x = torch.cat(
+    #     [
+    #         graph.x[0:idx_to_remove],
+    #         graph.x[idx_to_remove + 1:],
+    #     ]
+    # )
+    #
+    # np_edge_index = graph.edge_index.cpu().numpy()
+    # np_edge_weight = graph.edge_weight.cpu().numpy()
+    #
+    # # Select edges to remove that contained that node
+    # edges_to_keep = np.invert(
+    #     np.logical_or(np_edge_index[0] == idx_to_remove, np_edge_index[1] == idx_to_remove)
+    # )
+    #
+    # np_edge_weight = np_edge_weight[edges_to_keep]
+    # np_edge_index = np_edge_index[:, edges_to_keep]
+    #
+    # # Decrement index counter for all node with index greater than one deleted
+    # np_edge_index[np_edge_index > idx_to_remove] -= 1
+    #
+    # return new_x, np_edge_index, np_edge_weight
 
 
 def delete_node(batch_graph_data):
@@ -36,22 +61,34 @@ def delete_node(batch_graph_data):
     :param: graph_data: Torch geometric dataset for molecule
     :return: The updated graph with a node and corresponding edges removed
     """
+    batch_graph_data = Data.clone(batch_graph_data)
+
+    ptrs = batch_graph_data.ptr
+    random_ids = torch.rand(len(ptrs)-1)
+    len_graphs = ptrs[1:] - ptrs[:-1]
+
+    random_ids *= len_graphs
+    random_ids = torch.floor(random_ids)
+    random_ids += ptrs[:-1]
+
+    return _delete_node(batch_graph_data, random_ids)
+
     # Decompose batch of graphs to each composite graph
-    list_of_graphs = batch_graph_data.to_data_list()
+    # list_of_graphs = batch_graph_data.to_data_list()
 
-    new_list_of_graphs = []
-    for graph_data in list_of_graphs:
-        # Select random node to remove
-        random_idx_to_remove = np.random.randint(0, graph_data.x.size(dim=0))
-
-        new_x, np_edge_index, np_edge_weight = _delete_node(graph_data, random_idx_to_remove)
-
-        node_deleted_graph = Data(
-            x=new_x,
-            edge_index=torch.tensor(np_edge_index, dtype=torch.long),
-            edge_weight=torch.tensor(np_edge_weight, dtype=torch.float),
-        )
-        new_list_of_graphs.append(node_deleted_graph)
-
-    # Rebatch for faster inference
-    return Batch.from_data_list(new_list_of_graphs)
+    # new_list_of_graphs = []
+    # for graph_data in list_of_graphs:
+    #     # Select random node to remove
+    #     random_idx_to_remove = np.random.randint(0, graph_data.x.size(dim=0))
+    #
+    #     new_x, np_edge_index, np_edge_weight = _delete_node(graph_data, random_idx_to_remove)
+    #
+    #     node_deleted_graph = Data(
+    #         x=new_x,
+    #         edge_index=torch.tensor(np_edge_index, dtype=torch.long),
+    #         edge_weight=torch.tensor(np_edge_weight, dtype=torch.float),
+    #     )
+    #     new_list_of_graphs.append(node_deleted_graph)
+    #
+    # # Rebatch for faster inference
+    # return Batch.from_data_list(new_list_of_graphs)
--- tests/test_deletion.py
+++ tests/test_deletion.py
@@ -1,4 +1,5 @@
 from torch_geometric.loader import DataLoader
+from torch_geometric.data import Batch
 from polygnn import featurize as feat
 from polygnn import contrast as cst
 import torch
@@ -32,8 +33,6 @@ def test_node_deletion():
         feat.get_minimum_graph_tensor(x, bond_config, atom_config, "monocycle")
         for x in train_smiles
     ]
-    for gr in train_X:
-        print(gr)
 
     # loader = DataLoader(train_X, batch_size=1)
     # for graph in loader:
@@ -43,13 +42,12 @@ def test_node_deletion():
 
     # Check if the correct node was deleted based on specified index
     first_graph = train_X[0]
-    new_x, np_edge_index, np_edge_weight = cst.node_deletion._delete_node(first_graph, 0)
+    new_graph = cst.node_deletion._delete_node(Batch.from_data_list([first_graph]), torch.tensor([0]))
+    assert new_graph.x.shape == torch.Size([3, 70])
+    assert new_graph.edge_index.shape == (2, 4)
+    assert new_graph.edge_weight.shape == (4, 5)
 
-    assert new_x.shape == torch.Size([3, 70])
-    assert np_edge_index.shape == (2, 4)
-    assert np_edge_weight.shape == (4, 5)
-
-    assert np.array_equal(new_x.numpy(), torch.tensor([[1., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.,
+    assert np.array_equal(new_graph.x.numpy(), torch.tensor([[1., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.,
          0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.,
          0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 1., 0., 0., 0., 0., 0., 0., 0.,
          0., 0., 0., 1., 0., 0., 0., 0., 0., 0., 0., 0., 1., 0., 0., 0.],
@@ -61,23 +59,18 @@ def test_node_deletion():
          0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.,
          0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 1., 0., 0., 0., 0., 0., 0., 0.,
          0., 0., 0., 1., 0., 0., 0., 0., 0., 0., 0., 0., 1., 0., 0., 0.]]).numpy())
-    assert np.array_equal(np_edge_index, [[1, 2, 0, 1],[2, 1, 1, 0]])
-    assert np.array_equal(np_edge_weight, [[1., 0., 0., 0., 1.], [1., 0., 0., 0., 1.], [1., 0., 0., 0., 1.], [1., 0., 0., 0., 1.]])
+    assert np.array_equal(new_graph.edge_index, [[1, 2, 0, 1],[2, 1, 1, 0]])
+    assert np.array_equal(new_graph.edge_weight, [[1., 0., 0., 0., 1.], [1., 0., 0., 0., 1.], [1., 0., 0., 0., 1.], [1., 0., 0., 0., 1.]])
 
     # Check across batch if one node and coressponding edges are deleted
     loader = DataLoader(train_X, batch_size=4)
     # loader = DataLoader(train_X, batch_size=np.random.randint(1, 1+len(train_X)))
     for graphs in loader:
-        print(graphs)
-        print(graphs.edge_index)
-        print(graphs.batch)
-        print(graphs.ptr)
+        graphs_with_deleted_node = cst.node_deletion.delete_node(graphs)
+        assert len(graphs.x) - 4 == len(graphs_with_deleted_node.x)
+        assert len(graphs.edge_weight) - 14 == len(graphs_with_deleted_node.edge_weight)
 
-        print("BATCH DE")
-        for gr in graphs.to_data_list():
-            print(gr)
-        # graphs_with_deleted_node = cst.node_deletion.delete_node(graphs).to_data_list()
-        # for graph, graph_with_deleted_node in zip(graphs.to_data_list(), graphs_with_deleted_node):
+        # for graph, graph_with_deleted_node in zip(graphs.to_data_list(), graphs_with_deleted_node.to_data_list()):
         #     assert len(graph.x) - 1 == len(graph_with_deleted_node.x)
         #     assert len(graph.edge_weight) >= len(graph_with_deleted_node.edge_weight)
 
