Fixed mixed precision training and added other potential speedups

polygnn/contrast/train.py
@@ -11,27 +11,31 @@ from collections import deque
 import time
 
 
-def amp_train(model, view1, view2, optimizer, tc):
+def amp_train(model, view1, view2, optimizer, tc, apply_grad):
     """
     This function handles the parts of the per-epoch loop that torch's
     autocast methods can speed up. See https://pytorch.org/docs/1.9.1/notes/amp_examples.html
     """
     if tc.amp:
-        with amp.autocast(device_type=tc.device, enabled=True):
+        scaler = torch.cuda.amp.GradScaler()
+        with amp.autocast(enabled=True):
             output = model(view1, view2)
             loss = tc.loss_obj(output)
-            # Scales loss.  Calls backward() on scaled loss to create scaled gradients.
-            # Backward passes under autocast are not recommended.
-            # Backward ops run in the same dtype autocast chose for corresponding forward ops.
-            amp.scaler.scale(loss).backward()
 
-            # scaler.step() first unscales the gradients of the optimizer's assigned params.
-            # If these gradients do not contain infs or NaNs, optimizer.step() is then called,
-            # otherwise, optimizer.step() is skipped.
-            amp.scaler.step(optimizer)
+        # Scales loss.  Calls backward() on scaled loss to create scaled gradients.
+        # Backward passes under autocast are not r
+        # ecommended.
+        # Backward ops run in the same dtype autocast chose for corresponding forward ops.
+        scaler.scale(loss).backward()
+
+        # scaler.step() first unscales the gradients of the optimizer's assigned params.
+        # If these gradients do not contain infs or NaNs, optimizer.step() is then called,
+        # otherwise, optimizer.step() is skipped.
+        if apply_grad:
+            scaler.step(optimizer)
 
             # Updates the scale for next iteration.
-            amp.scaler.update()
+            scaler.update()
     else:
         output = model(view1, view2)
         loss = tc.loss_obj(output)
@@ -71,7 +75,7 @@ def train(
             raise ValueError(f"The model_save_path you passed in does not end in .pt")
     # create the epoch suffix for this submodel
     epoch_suffix = f"{cfg.epoch_suffix}, fold {cfg.fold_index}"
-    model.to(cfg.device)
+    model.to(cfg.device, non_blocking=True)
     optimizer = optim.Adam(
         model.parameters(), lr=cfg.hps.r_learn.value
     )  # Adam optimization
@@ -104,6 +108,8 @@ def train(
             pin_memory=True,
             num_workers=10,
         )
+    # Enable benchmarking for optimal function execution
+    torch.backends.cudnn.benchmark = True
     start = time.time()
     for epoch in range(cfg.epochs):
         # Let's stop training and not waste time if we have vanishing
@@ -141,15 +147,21 @@ def train(
         model.train()
         epoch_tr_loss = 0
         for ind, data in enumerate(train_loader):
-            data = data.to(cfg.device)
+            data = data.to(cfg.device, non_blocking=True)
             with torch.no_grad():
                 view1, view2 = data.clone(), data.clone()
                 del data  # save space
                 for fn in transforms:
                     view1, view2 = fn(view1), fn(view2)
-            optimizer.zero_grad(set_to_none=True)
-            _, loss_item = amp_train(model, view1, view2, optimizer, cfg)
+            if (ind+1) % 4 == 0 or (ind+1) == len(train_loader):
+                apply_grad = True
+            else:
+                apply_grad = False
+            if apply_grad == False:
+                optimizer.zero_grad(set_to_none=True)
+            _, loss_item = amp_train(model, view1, view2, optimizer, cfg, apply_grad)
             epoch_tr_loss += loss_item
+            
         with torch.no_grad():
             epoch_tr_loss = epoch_tr_loss / len(train_pts)
             # ################################################################
@@ -158,7 +170,7 @@ def train(
             model.eval()
             epoch_val_loss = 0
             for ind, data in enumerate(val_loader):
-                data = data.to(cfg.device)
+                data = data.to(cfg.device, non_blocking=True)
                 view1, view2 = transforms[0](data.clone()), transforms[0](data.clone())
                 del data  # save space
                 for fn in transforms[1:]: